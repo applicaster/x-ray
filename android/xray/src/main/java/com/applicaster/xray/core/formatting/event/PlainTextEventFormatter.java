abb737fefc516af2ce3c188e04170c302a812dcb
Alex <24409942+inckie@users.noreply.github.com>
2020-08-07 13:37:50 -0400

    + better log file text output

--- android/xray/src/main/java/com/applicaster/xray/core/formatting/event/PlainTextEventFormatter.java
+++ android/xray/src/main/java/com/applicaster/xray/core/formatting/event/PlainTextEventFormatter.java
@@ -1,19 +1,28 @@
 package com.applicaster.xray.core.formatting.event;
 
+import android.text.format.DateFormat;
+
 import androidx.annotation.NonNull;
 
 import com.applicaster.xray.core.Event;
+import com.applicaster.xray.core.LogLevel;
 
 import java.util.Map;
 
 public class PlainTextEventFormatter implements IEventFormatter {
 
-    // todo: set format string (ADB Sink does not need tag or time)
+    // todo: add format string option
 
     @NonNull
     @Override
     public String format(@NonNull Event event) {
-        StringBuilder sb = new StringBuilder(event.getMessage()).append("\n");
+        StringBuilder sb = new StringBuilder()
+                .append(DateFormat.format("yyyy-MM-dd HH:mm:ss", event.getTimestamp())).append(" ")
+                .append(LogLevel.fromLevel(event.getLevel()).name()).append(" ")
+                .append(event.getCategory()).append(" ")
+                .append(event.getSubsystem()).append(" ")
+                .append(event.getMessage())
+                .append("\n");
         Map<String, Object> data = event.getData();
         if(null != data && !data.isEmpty()) {
             sb.append("\n\tdata: ").append(data).append("\n");
